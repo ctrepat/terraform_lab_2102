6f6dac0e957b83670f8cd02c97aa0a53c39e8328
Carlos Trepat <carlostrepat@gmail.com>
2021-06-26 21:27:23 +1200

    nuevas instancias agregadas

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
 provider "aws" {
   version = "~> 2.7"
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "web" {  
-  ami = "ami-077e31c4939f6a2f3"
+resource "aws_instance" "web" {
+  count = 2
+  ami = "ami-0ac80df6eff0e70b5"
   instance_type = "t2.micro"
+  
+  tags = {
+    Name = "devops-${count.index}"
+  }
 }
 
 output "public_ip" {
-  value = "${aws_instance.web.public_ip}"
+  value = "${aws_instance.web.*.public_ip}"
 }
+
+output "private_ip" {
+  value = "${aws_instance.web.*.private_ip}"
+}
+
